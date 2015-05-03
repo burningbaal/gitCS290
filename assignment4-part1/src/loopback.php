b63c53a98b436471cbf010c6471a58ded6c04d02
burningbaal <mckinleykj@gmail.com>
2015-05-03 16:16:32 -0700

    finished loopback.php
    Start: Sun, 03 May 2015 14:45 -0800
    End: Sun, 03 May 2015 16:15 -0800
    Tasks:  Got loopback.php written and working.  waiting for clairity
    about handling both a GET and POST at the same time.

--- assignment4-part1/src/loopback.php
+++ assignment4-part1/src/loopback.php
@@ -14,4 +14,44 @@ You are welcome to use built in JSON function in PHP to produce this output.
 http://php.net/manual/en/function.json-encode.php
 **********************/
 
+//http://stackoverflow.com/questions/6431106/count-values-in-post
+//simple way to retrieve the number of parameters
+header('Content-Type: application/json');
+
+$numGet = count($_GET);
+$numPost = count($_POST);
+
+$output = "{";
+if ($numGet == 0)
+{
+	$output .= '"Type":"GET","parameters":null}';
+}
+else 
+{
+	$output .= '{"Type":"GET","parameters":{';
+	$strGet = $_SERVER['QUERY_STRING']; //http://stackoverflow.com/questions/3141708/php-easiest-way-to-get-get-parameters-string-as-written-in-the-url
+	$arrGet = explode("&", $strGet); http://stackoverflow.com/questions/4909830/php-split-string-into-2d-array
+	foreach($arrGet as &$arrVal)
+	{	
+		$arrVal = explode(":", $arrVal);
+		foreach($arrVal as &$strVal){
+			$strVal = '"' . $strVal . '"';
+		} 
+	}
+	
+	//test that explode/implode worked:
+	foreach($arrGet as &$arrVal) $arrVal = implode(":", $arrVal);
+	$output .= implode(", ", $arrGet);
+	$output .= "}}";
+	}
+if ($numPost == 0)
+{
+	$output .= '{"Type":"POST","parameters":null}';
+}
+else{
+	
+	$output .= '{"Type":"POST","parameters":';
+	$output .= json_encode($_POST) . "}";
+}
 ?>
+	<?php echo $output; ?>
