24b70d0955dc95c93c67bb088bf94f075d73f3ed
burningbaal <mckinleykj@gmail.com>
2015-05-03 16:58:39 -0700

    done!
    Finished loopback to properly check for missing request methods.  tested
    with https://www.hurl.it/ and http://jsonformatter.curiousconcept.com/

--- assignment4-part1/src/loopback.php
+++ assignment4-part1/src/loopback.php
@@ -22,36 +22,50 @@ $numGet = count($_GET);
 $numPost = count($_POST);
 
 $output = "";
-if ($numGet == 0)
+
+if ($numPost == 0 && strlen($_SERVER['QUERY_STRING']) < 1) 
 {
-	$output .= '{"Type":"GET","parameters":null}';
+	$output .= '{"Type":"null","parameters":null}';
 }
-else 
+elseif ($numGet > 0 )
 {
-	$output .= '{"Type":"GET","parameters":{';
-	$strGet = $_SERVER['QUERY_STRING']; //http://stackoverflow.com/questions/3141708/php-easiest-way-to-get-get-parameters-string-as-written-in-the-url
-	$arrGet = explode("&", $strGet); http://stackoverflow.com/questions/4909830/php-split-string-into-2d-array
-	foreach($arrGet as &$arrVal)
-	{	
-		$arrVal = explode(":", $arrVal);
-		foreach($arrVal as &$strVal){
-			$strVal = '"' . $strVal . '"';
-		} 
+	if($_GET[0] == "" )
+	{
+		$output = '{"Type":"GET","parameters":null}';
 	}
-	
-	//test that explode/implode worked:
-	foreach($arrGet as &$arrVal) $arrVal = implode(":", $arrVal);
-	$output .= implode(", ", $arrGet);
-	$output .= "}}";
+	else{
+		$output .= '{"Type":"GET","parameters":{';
+		$strGet = $_SERVER['QUERY_STRING']; //http://stackoverflow.com/questions/3141708/php-easiest-way-to-get-get-parameters-string-as-written-in-the-url
+		$arrGet = explode("&", $strGet); http://stackoverflow.com/questions/4909830/php-split-string-into-2d-array
+		foreach($arrGet as &$arrVal)
+		{	
+			$arrVal = explode(":", $arrVal);
+			foreach($arrVal as &$strVal){
+				$strVal = '"' . $strVal . '"';
+			} 
+		}
+		
+		//test that explode/implode worked:
+		foreach($arrGet as &$arrVal) $arrVal = implode(":", $arrVal);
+		$output .= implode(", ", $arrGet);
+		$output .= "}}";
 	}
-if ($numPost == 0)
-{
-	$output .= '{"Type":"POST","parameters":null}';
 }
-else{
-	
-	$output .= '{"Type":"POST","parameters":';
-	$output .= json_encode($_POST) . "}";
+elseif ($numPost > 0)
+{
+	if($_POST[0] == "")
+	{
+		$output = '{"Type":"POST","parameters":null}';
+	}
+	else
+	{
+		$output .= '{"Type":"POST","parameters":';
+		$output .= json_encode($_POST) . "}";
+	}
+}
+else
+{
+	print("empty");
 }
 ?>
 	<?php echo $output; ?>
