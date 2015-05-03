e49b0e2bd739cb84e8764a4ee69392d459aec7d3
burningbaal <mckinleykj@gmail.com>
2015-05-03 12:30:54 -0700

    got multtable.php working
    seems to suit all the needs of the requirement for multtable.  nothing
    done on loopback, and still need to finish content1's visit counter and
    link to content2

--- assignment4-part1/src/multtable.php
+++ assignment4-part1/src/multtable.php
@@ -30,3 +30,127 @@ To accomplish the above task you will want to work with loops to dynamically cre
 **********************/
 
 ?>
+<?php 	$allOk = "";
+			if ( empty($_GET['min-multiplicand']) )
+			{
+				$allOk .= "Missing parameter min-multiplicand <br>";
+			} else {
+				$min1 = $_GET['min-multiplicand'];
+				if ( is_numeric($min1) && intval($min1) == $min1) //http://php.net/manual/en/function.intval.php, evaluate is_numeric first because intval can return an integer if it's not a number
+				{
+					$min1 = intVal($min1);
+					//print("min-multiplicand: " . $min1 . "<br>");
+				}else {
+					$allOk .= "min-multiplicand must be an integer. <br>";
+				}
+			} 
+			if ( empty($_GET['max-multiplicand']) )
+			{
+				$allOk .= "Missing parameter max-multiplicand<br>";
+			}else {
+				$max1 = $_GET['max-multiplicand'];
+				if ( is_numeric($max1) && intval($max1) == $max1) 
+				{
+					$max1 = intVal($max1);
+					//print("max-multiplicand: " . $max1 . "<br>");
+					if ($max1 <= $min1)
+					{
+						$allOk .= "Minimum multiplicand larger than maximum.<br>";
+					}
+				}else {
+					$allOk .= "max-multiplicand must be an integer. <br>";
+				}
+			}
+			if ( empty($_GET['min-multiplier']) )
+			{
+				$allOk .= "Missing parameter min-multiplier <br>";
+			} else {
+				$min2 = $_GET['min-multiplier'];
+				if ( is_numeric($min2) && intval($min2) == $min2)
+				{
+					$min2 = intVal($min2);
+					//print("min-multiplier: " . $min2 . "<br>");
+				}else {
+					$allOk .= "min-multiplier must be an integer. <br>";
+				}
+			}
+			if ( empty($_GET['max-multiplier']) )
+			{
+				$allOk .= "Missing parameter max-multiplier";
+			} else {
+				$max2 = $_GET['max-multiplier'];
+				if ( is_numeric($max2) && intval($max2) == $max2) 
+				{
+					$max2 = intVal($max2);
+					//print("max-multiplier: " . $max2 . "<br>");
+					if ($max2 <= $min2)
+					{
+						$allOk .= "Minimum multiplier larger than maximum.<br>";
+					}
+				}else {
+					$allOk .= "max-multiplier must be an integer. <br>";
+					if ($max2 <= $min2 )
+					{
+						$allOk .= "Minimum multiplier larger than maximum.";
+					}
+				}
+			}
+			?>
+<html>
+<head>
+	<style type="text/css">
+		h1: {font-size: 2em;}
+		table {border-collapse: collapse;}
+		th, td {
+			border: 1px solid black;
+			/*width: <?php print(100 / ($max2 - $min2 + 3)); ?>%;*/
+			padding: 5px;
+			height: 20px;
+			text-align: center;
+			vertical-align: middle; 
+		}
+		th {
+			border: 1px solid black;
+			font-weight: bold;
+		}
+	</style>
+</head>
+
+<h1>This is the multiplication table!</h1>
+<body>
+	
+	<?php if($allOk === "") : ?>
+	<table id="multtable" >
+		<tr>
+			<th> </th>
+			<?php for($cols = $min2; $cols <= ($max2); $cols ++)
+				//print top row
+			{
+			echo "<th> $cols </th>";
+			} ?>
+		</tr>
+		<?php for($rows = $min1; $rows < ($max1 + 1); $rows ++ )
+					//print each row
+		{ 
+			echo "<tr>
+				";
+			for($cols = $min2 - 1; $cols < ($max2 + 1); $cols ++ )
+				//print each col
+			{
+				if ($cols < $min2 ){ 
+					echo "<th> $rows </th>";
+				 }
+				 else {
+					echo "<td>" . $rows * $cols . "</td>"; 
+				 }
+			} 
+			echo "</tr>
+				";
+		} ?>
+		
+	</table>
+	<?php else : ?>
+		<p><?php echo "<br> $allOk <br>"; ?></p>
+	<?php endif; ?>
+</body>
+</html>
